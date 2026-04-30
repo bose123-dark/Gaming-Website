3e8a09aa07604e16a93bb28f25bd58533a3b90f3
Bose <boseam123@gmail.com>
2026-04-30 20:11:20 +0530

    remove superuser from build

--- build.sh
+++ build.sh
@@ -4,5 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-python manage.py createsuperuser --noinput --username admin --email admin@admin.com
