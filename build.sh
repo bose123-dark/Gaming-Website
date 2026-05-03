68ba12e6e80f7c9afb10c5f299e4109271e78b6a
Bose <boseam123@gmail.com>
2026-05-03 23:35:34 +0530

    force clear staticfiles on render

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+rm -rf staticfiles
 mkdir -p staticfiles
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
 python manage.py migrate
