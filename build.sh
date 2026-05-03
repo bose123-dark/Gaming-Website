575e1cd3721f8a58fe37d830d211412be66843c0
Bose <boseam123@gmail.com>
2026-05-03 22:23:38 +0530

    fix static files

--- build.sh
+++ build.sh
@@ -4,5 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-python manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.get(username='admin'); u.set_password('Game@1234'); u.save()"
